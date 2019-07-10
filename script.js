9827091bd09463c0c46e5035141910517aa03ff2
Raj Sharma <raj.x82@gmail.com>
2019-07-09 21:54:28 -0500

    Get colors working

--- script.js
+++ script.js
@@ -8,9 +8,12 @@ var vertexShaderSource = `#version 300 es
 // an attribute is an input (in) to a vertex shader.
 // It will receive data from a buffer
 in vec2 a_position;
+in vec4 a_color;
 
 // Used to pass in the resolution of the canvas
 uniform vec2 u_resolution;
+
+out vec4 v_color;
 
 // all shaders have a main function
 void main() {
@@ -25,6 +28,9 @@ void main() {
   vec2 clipSpace = zeroToTwo - 1.0;
 
   gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
+
+  // Pass through the color unmodified
+  v_color = a_color;
 }
 `;
 
@@ -32,13 +38,13 @@ var fragmentShaderSource = `#version 300 es
 
 precision mediump float;
 
-uniform vec4 u_color;
+in vec4 v_color;
 
 // we need to declare an output for the fragment shader
 out vec4 outColor;
 
 void main() {
-  outColor = u_color;
+  outColor = v_color;
 }
 `;
 
@@ -79,7 +85,7 @@ class RectangleDrawer {
         const gl = this.getContext();
         this.program = webglUtils.createProgramFromSources(gl, [vertexShaderSource, fragmentShaderSource]);
         this.rectangles = [];
-        for (let i = 0; i < 100000; ++i){
+        for (let i = 0; i < 1000; ++i){
             const x = randomInt(1000);
             const y = randomInt(1000);
             this.rectangles.push([x, y, 20, 20, Math.random(), Math.random(), Math.random()]);
@@ -90,6 +96,22 @@ class RectangleDrawer {
         var canvas = document.getElementById("c");
         var gl = canvas.getContext("webgl2");
         return gl;
+    }
+
+    /**
+     * Given color components as floating point from 0.0 to 1.0, return a packed 32-bit
+     * floating point value.
+     */
+    getFloatColor(r, g, b, a)
+    {
+        // Get uint8 versions of each component
+        const r_8 = Math.floor(r * 255);
+        const g_8 = Math.floor(g * 255);
+        const b_8 = Math.floor(b * 255);
+        const a_8 = Math.floor(a * 255);
+
+        // Use TypedArray casting to pack the 8-bit values into a single 32-bit value
+        return new Float32Array((new Uint8Array([r_8, g_8, b_8, a_8])).buffer)[0];
     }
 
     draw() {
@@ -103,13 +125,14 @@ class RectangleDrawer {
 
         // look up where the vertex data needs to go.
         var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+        var colorAttributeLocation = gl.getAttribLocation(program, "a_color");
 
         // look up uniform locations
         var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
         var colorLocation = gl.getUniformLocation(program, "u_color");
 
         // Create a buffer
-        var positionBuffer = gl.createBuffer();
+        var buffer = gl.createBuffer();
 
         // Create a vertex array object (attribute state)
         var vao = gl.createVertexArray();
@@ -117,20 +140,19 @@ class RectangleDrawer {
         // and make it the one we're currently working with
         gl.bindVertexArray(vao);
 
-        // Turn on the attribute
+        // Turn on the attributes
         gl.enableVertexAttribArray(positionAttributeLocation);
+        gl.enableVertexAttribArray(colorAttributeLocation);
 
-        // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
-        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+        // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = buffer)
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
 
-        // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-        var size = 2;          // 2 components per iteration
-        var type = gl.FLOAT;   // the data is 32bit floats
-        var normalize = false; // don't normalize the data
-        var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
-        var offset = 0;        // start at the beginning of the buffer
+        // Tell the attribute how to get data out of buffer (ARRAY_BUFFER)
+        const stride = 12;
         gl.vertexAttribPointer(
-            positionAttributeLocation, size, type, normalize, stride, offset);
+            positionAttributeLocation, 2, gl.FLOAT, false, stride, 0);
+        gl.vertexAttribPointer(
+            colorAttributeLocation, 4, gl.UNSIGNED_BYTE, true, stride, 8);
 
         webglUtils.resizeCanvasToDisplaySize(gl.canvas);
 
@@ -154,27 +176,46 @@ class RectangleDrawer {
         const offsetX = 30 * Math.cos(performance.now() / 1000.0);
         const offsetY = 30 * Math.sin(performance.now() / 1000.0);
 
+        const numTrianglesPerRectangle = 2;
+        const numVerticesPerTriangle = 3;
+        const numFloatsPerVertex = 3; // 2 floats for position + 1 float (4 ubytes) for color
+        const numFloatsPerTriangle = numFloatsPerVertex * numVerticesPerTriangle;
+        const numFloatsPerRectangle = numFloatsPerTriangle * numTrianglesPerRectangle;
+        const arrayBuffer = new Float32Array(numFloatsPerRectangle * this.rectangles.length);
+        let curIndex = 0;
+
+        const addVertex = (x, y, color) => {
+            arrayBuffer[curIndex++] = x;
+            arrayBuffer[curIndex++] = y;
+            arrayBuffer[curIndex++] = color;
+        };
+
+        const addRectangle = (x, y, w, h, r, g, b) => {
+            const x2 = x + w;
+            const y2 = y + h;
+            const color = this.getFloatColor(r, g, b, 1.0);
+            addVertex(x, y, color);
+            addVertex(x2, y, color);
+            addVertex(x, y2, color);
+            addVertex(x, y2, color);
+            addVertex(x2, y, color);
+            addVertex(x2, y2, color);
+        };
         // draw random rectangles in random colors
-        const coordinates = [];
-        let first = true;
         for (const rect of this.rectangles) {
             // Put a rectangle in the position buffer
             const [x, y, w, h, r, g, b] = rect;
-            appendRectangle(coordinates, x + offsetX, y + offsetY, w, h);
-
-            // Set a random color.
-            if (first) {
-                gl.uniform4f(colorLocation, r, g, b, 1);
-                first = false;
-            }
+            addRectangle(x + offsetX, y + offsetY, w, h, r, g, b);
         }
 
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(coordinates), gl.STATIC_DRAW);
+        // Copy the coordinates into the currently bound buffer.
+        gl.bufferData(gl.ARRAY_BUFFER, arrayBuffer, gl.STATIC_DRAW);
 
         // Draw the rectangle.
         var primitiveType = gl.TRIANGLES;
         var offset = 0;
-        var count = 6 * this.rectangles.length;
+        const numVerticesPerRectangle = numVerticesPerTriangle * numTrianglesPerRectangle;
+        var count = numVerticesPerRectangle * this.rectangles.length;
         gl.drawArrays(primitiveType, offset, count);
     }
 }
